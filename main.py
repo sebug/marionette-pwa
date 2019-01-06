fc2f34f685b2ae49c9f988afdc580f3282200175
sebug <sebastian.gfeller@gmail.com>
2019-01-06 15:18:32 +0100

    first class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!python
 
 from marionette_driver.marionette import Marionette
+from pages import InformationPage
 
 def get_first_url():
     urls = open("urls.txt", "r")
@@ -11,7 +12,9 @@ def navigate(url):
     client = Marionette(host='localhost', port=2828)
     client.start_session()
     client.navigate(url)
-    print url
+    start_page = InformationPage(client)
+    start_page.print_menu_items()
+
 
 
 if __name__ == "__main__":
---
+++ pages.py
@@ -0,0 +1,14 @@
+#!python
+from marionette_driver import By
+
+class InformationPage:
+    """A progressive information page, gives access to the components"""
+    def __init__(self, client):
+        self.client = client
+
+    def print_menu_items(self):
+        items = self.client.find_elements(By.CSS_SELECTOR, ".menu a.item")
+        for item in items:
+            if item.text:
+                print item.text
+
