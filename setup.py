5b069c1f02ef24805d09cceeb783d9c793791508
sebug <sebastian.gfeller@gmail.com>
2019-01-06 15:27:23 +0100

    Adding setup.py, requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!python
+from distutils.core import setup
+
+setup(name='marionette-pwa',
+      version='0.1',
+      description='Marionetting the PWA',
+      author='Sebastian Gfeller',
+      author_email='sebastian.gfeller@gmail.com',
+      url='https://github.com/sebug/marionette-pwa')
+
